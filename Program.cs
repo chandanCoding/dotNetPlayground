1c12c72cd9183818c8a5afafcc16c90dd7c98530
Chandan Swarup Swain <chandan.swarup1@gmail.com>
2020-01-11 19:42:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ConsoleSketch
+{
+    public class MySketch
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Beginning Execution...");
+            SketchModel output = TestAnonymous();
+            var JsonRes = JsonSerializer.Serialize(output);
+            Console.WriteLine(JsonRes);
+            Console.WriteLine("Processing Successful");
+        }
+        public static SketchModel TestAnonymous()
+        {
+            List<object> MyList1 = new List<object>();
+            List<object> MyList2 = new List<object>();
+            List<object> MyList3 = new List<object>();
+            MyList1.Add("MyValue1");
+            MyList2.Add("MyValue2");
+            MyList3.Add("MyValue3");
+
+            return new SketchModel()
+            {
+                MyList1 = MyList1,
+                MyList2 = MyList2,
+                MyList3 = MyList3,
+                AnonymousBoolean = true
+            };
+        }
+    }
+}
---
+++ SketchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleSketch
+{
+    public class SketchModel
+    {
+        public List<object> MyList1 { get; set; }
+        public List<object> MyList2 { get; set; }
+        public List<object> MyList3 { get; set; }
+        public bool AnonymousBoolean { get; set; }
+    }
+}
